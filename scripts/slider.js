9ed485c0abce1f55ec04b4e085ed76d97a8c476c
Nickolaj <nikolajpetrishchev@gmail.com>
2023-10-05 20:57:26 +0600

    update

--- scripts/slider.js
+++ scripts/slider.js
@@ -7,4 +7,25 @@ document.addEventListener('DOMContentLoaded', function() {
             prevEl: '.button-prev',
         },
     });
+    const breakpoint = window.matchMedia('(min-width: 768px)');
+
+    const breakpointChecker = function() {
+        if (breakpoint.matches) {
+            // На больших экранах, устанавливаем другое количество видимых слайдов
+            swiper.params.slidesPerView = 3.5; // Например, 3 видимых слайда
+        } else {
+            // На меньших экранах, устанавливаем другое количество видимых слайдов
+            swiper.params.slidesPerView = 1.3; // Например, 1.3 видимых слайда
+            swiper.params.spaceBetween = 10;
+        }
+
+        // Обновляем Swiper с новыми параметрами
+        swiper.update();
+    };
+
+    // Добавляем обработчик изменения разрешения экрана
+    breakpoint.addListener(breakpointChecker);
+
+    // Запускаем функцию проверки разрешения на старте
+    breakpointChecker();
 });
